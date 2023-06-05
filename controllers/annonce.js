66588b161127b6d8f305eb452a0fa611e6ef8a63
kousaila502 <k.benhamouche@esi-sba.dz>
2023-06-05 14:27:59 +0200

    changes

--- controllers/annonce.js
+++ controllers/annonce.js
@@ -12,13 +12,22 @@ const createAnnonce = async (req, res) => {
   };
 
 const getAllAnnonce = async (req, res) => {
+
+    const { status } = req.query;
+    const queryObject = {};
+  
+    if(status){
+      queryObject.status = status;
+    }
     const page = Number(req.query.page) || 1;
     const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
+  
+    const result = await Annonce.find(queryObject).skip(skip).limit(limit);
+  
+    const totalCount = await Annonce.countDocuments(queryObject);
+    res.status(StatusCodes.OK).json({ result, count: result.length , totalCount });
 
-    const result = await Annonce.find({}).skip(skip).limit(limit);
-
-    res.status(StatusCodes.OK).json({ result, count: result.length });
 };
 
 const getSingleAnnonce = async (req, res) => {
