615b8af47601f952167b787c4726b48ccb3182e2
kousaila502 <k.benhamouche@esi-sba.dz>
2023-06-08 12:45:21 +0200

    ch

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,7 @@ const getAllUsers = async (req, res) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 10;
   const skip = (page - 1) * limit;
-  const users = await User.find({ role: 'user' }).select('-password').skip(skip).limit(limit);
+  const users = await User.find({ role: 'user' }).select('-password').skip(skip).limit(limit).sort('-createdAt');
   const totalCount = await User.countDocuments({ role: 'user' });
 
   res.status(StatusCodes.OK).json({ users, count: users.length , totalCount });
